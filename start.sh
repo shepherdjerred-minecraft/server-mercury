b7ee61f28accb85d8298b45c55d13253359ac5ee
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-05-25 13:15:53 -0700

    Make start script interactive

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sudo docker run -p 25565:25565/tcp -p 25565:25565/udp -p 8123:8123 --mount type=bind,source=/server/data,target=/home/minecraft/server shepherdjerred/spigot:latest
+sudo docker run -p 25565:25565/tcp -p 25565:25565/udp -p 8123:8123 --mount type=bind,source=/server/data,target=/home/minecraft/server shepherdjerred/spigot:latest --interactive
 
