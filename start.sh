dcdca625b6f8a6facd1ec62c16bba01f719852a7
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-05-25 13:21:48 -0700

    Wrap docker in tmux

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-sudo docker run -p 25565:25565/tcp -p 25565:25565/udp -p 8123:8123 --mount type=bind,source=/server/data,target=/home/minecraft/server shepherdjerred/spigot:latest --interactive
-
+tmux new -d -s minecraft
+tmux send-keys "sudo docker run -p 25565:25565/tcp -p 25565:25565/udp -p 8123:8123 --mount type=bind,source=/server/data,target=/home/minecraft/server --interactive shepherdjerred/spigot:latest"
+tmux send-keys Enter
+tmux attach -t minecraft
